9470ff65ddf6c3481d3c3ab6acc965999c56a807
EzeBalcaldi <42391879+EzeBalcaldi@users.noreply.github.com>
2020-10-05 04:13:12 -0300

    Update main.js

--- WebContent/main.js
+++ WebContent/main.js
@@ -9,7 +9,7 @@ document.querySelector("#getReporte").addEventListener("click", getReporte);
 
 
 
- function postEstudiante(){
+ function postEstudiante(){ //Método para postear un estudiante, agarramos los values de los inputs de la pagina, creamos un objeto con esos datos y lo subimos a la base
 let nombre = document.querySelector("#idNombreInsert").value;
 let apellido = document.querySelector("#idApellidoInsert").value;
 let edad = document.querySelector("#idEdadInsert").value;
@@ -41,7 +41,7 @@ let estudiante = {
 
 
 
-async function getEstudiantes(){
+async function getEstudiantes(){ //Método para obtener los estudiantes, le pedimos los datos a la base mediante un protocolo HTTP GET y lo volcamos en el HTML
   let  url = "entregable/estudiante/recuperarEstudiantes";
 let r = await fetch(url, {
      'method': 'GET',
@@ -54,7 +54,7 @@ contenedor.innerHTML = JSON.stringify(json);
 }
 
 
-async function getCarreraCiudad(){
+async function getCarreraCiudad(){//Método para obtener los estudiantes filtrados por carrera y ciudad, le pedimos los datos a la base mediante un protocolo HTTP GET y lo volcamos en el HTML
   let idCarrera = document.querySelector("#idCarrera").value;
   let ciudad = document.querySelector("#Ciudad").value;
 
@@ -69,7 +69,7 @@ let contenedor = document.querySelector("#contenedorCarreraCiudad");
 contenedor.innerHTML = JSON.stringify(json);
 }
 
-async function getCarrerasSegunInscriptos(){
+async function getCarrerasSegunInscriptos(){ //Método para obtener las carreras, le pedimos los datos a la base mediante un protocolo HTTP GET y lo volcamos en el HTML
   let  url = "entregable/carrera/carrerasSegunInscriptos";
 let r = await fetch(url, {
      'method': 'GET',
@@ -81,7 +81,7 @@ let contenedor = document.querySelector("#contenedorCarrerasSegunInscriptos");
 contenedor.innerHTML = JSON.stringify(json);
 }
 
-async function getReporte(){
+async function getReporte(){ //Método para obtener los reportes de las carreras, le pedimos los datos a la base mediante un protocolo HTTP GET y lo volcamos en el HTML
   let  url = "entregable/carrera/reporteCarreras";
 let r = await fetch(url, {
      'method': 'GET',
@@ -93,7 +93,7 @@ let contenedor = document.querySelector("#contenedorReporte");
 contenedor.innerHTML = JSON.stringify(json);
 }
 
-async function getEstudiantesLibreta(){
+async function getEstudiantesLibreta(){ //Método para obtener los estudiantes filtrados por su numero de libreta, le pedimos los datos a la base mediante un protocolo HTTP GET y lo volcamos en el HTML
   let libreta = document.querySelector("#valueLibreta").value;
   let  url = "entregable/estudiante/estudianteSegunLibreUniversitaria/" + libreta;
 let r = await fetch(url, {
@@ -106,7 +106,7 @@ let contenedor = document.querySelector("#contenedorEstudiantesLibreta");
 contenedor.innerHTML = JSON.stringify(json);
 }
 
-async function getEstudiantesGenero() {
+async function getEstudiantesGenero() { //Método para obtener los estudiantes filtrados por su género, le pedimos los datos a la base mediante un protocolo HTTP GET y lo volcamos en el HTML
     let genero = document.querySelector("#valueGenero").value;
     let url = "entregable/estudiante/estudianteSegunGenero/" + genero;
     let r = await fetch(url, {
